6d2ad5f9c6d2a3d2b765f27d4a0b8895f255c8e5
Enrique Gongora <EGTech24@gmail.com>
2020-05-25 17:02:29 -0700

    Completed the Onboarding View Controller. Created all IBOutlets/Actions. Added methods to update the UI based off what index you are on. Added segue navigation for the page controller

--- Financify/Financify/View Controllers/OnboardingViewController.swift
+++ Financify/Financify/View Controllers/OnboardingViewController.swift
@@ -8,17 +8,74 @@
 
 import UIKit
 
-class OnboardingViewController: UIViewController {
-
+class OnboardingViewController: UIViewController, OnboardingPageViewControllerDelegate {
+    
     // MARK: - Properties
+    weak var onboardingPageViewController: OnboardingPageViewController?
     
     // MARK: - IBOutlets
-    
+    @IBOutlet weak var skipButton: UIButton!
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var nextButton: UIButton! {
+        didSet {
+            nextButton.layer.cornerRadius = 25.0
+            nextButton.layer.masksToBounds = true
+        }
+    }
     
     // MARK: - IBActions
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+        if let index = onboardingPageViewController?.currentIndex {
+            switch index {
+            case 0:
+                onboardingPageViewController?.forwardPage()
+            case 1:
+                onboardingPageViewController?.forwardPage()
+            case 2:
+                onboardingPageViewController?.forwardPage()
+            case 3:
+                UserDefaults.standard.set(true, forKey: "hasViewedOnboarding")
+                dismiss(animated: true)
+            default:
+                break
+            }
+        }
+        updateUI()
+    }
+    
+    // MARK: - Methods
+    func didUpdatePageIndex(currentIndex: Int) {
+        updateUI()
+    }
+    
+    func updateUI() {
+        if let index = onboardingPageViewController?.currentIndex {
+            switch index { // TODO - Add cases for allowing notifications
+            case 0:
+                nextButton.setTitle("Next", for: .normal)
+                skipButton.isHidden = false
+            case 1:
+                nextButton.setTitle("Next", for: .normal)
+                skipButton.isHidden = false
+            case 2:
+                nextButton.setTitle("Next", for: .normal)
+            default:
+                break
+            }
+            pageControl.currentPage = index
+        }
+    }
     
     // MARK: - View LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let pageViewController = segue.destination as? OnboardingPageViewController {
+            onboardingPageViewController = pageViewController
+            onboardingPageViewController?.onboardingPageViewDelegate = self
+        }
     }
 }
