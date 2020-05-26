aa8e0283dcf337da08979dd59f74d395b12a4a99
Chris <chris.gonzales.82@gmail.com>
2020-05-26 13:11:06 -0700

    Pull before push

---
+++ Financify/Financify/Model Controllers/BudgetController.swift
@@ -0,0 +1,21 @@
+//
+//  BudgetController.swift
+//  Financify
+//
+//  Created by Chris Gonzales on 5/26/20.
+//  Copyright © 2020 Chris Gonzales. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
+//class BudgetController {
+//    
+//    let ckManager: CloudKitManager?
+//    
+//    func fetchAllBudgetsFromCloudKit(completion: @escaping () -> Void) {
+//        guard let ckManager = ckManager else { return }
+//        
+//        ckManager.fetchRecordsOf(type: Budget., database: <#T##CKDatabase#>, completion: <#T##([CKRecord]?, Error?) -> Void#>)
+//    }
+//}
