1852ba0960353f30491e08e8d298a1b26e7d8306
Chris <chris.gonzales.82@gmail.com>
2020-05-26 09:17:53 -0700

    pulling changes

---
+++ Financify/Financify/Helper Files/CKRecordZone.ID+Dictionary.swift
@@ -0,0 +1,9 @@
+//
+//  CKRecordZone.ID+Dictionary.swift
+//  Financify
+//
+//  Created by Chris Gonzales on 5/25/20.
+//  Copyright © 2020 Chris Gonzales. All rights reserved.
+//
+
+import Foundation
---
+++ Financify/Financify/Model Controllers/ShareController.swift
@@ -0,0 +1,9 @@
+//
+//  ShareController.swift
+//  Financify
+//
+//  Created by Chris Gonzales on 5/24/20.
+//  Copyright © 2020 Chris Gonzales. All rights reserved.
+//
+
+import Foundation
