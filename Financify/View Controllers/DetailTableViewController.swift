acf0eb627a531a9f3c422717ff7654f7a1ecfc27
Enrique Gongora <EGTech24@gmail.com>
2020-05-27 20:39:05 -0700

    Created a UIAlert that will display 2 textfield for the user to input the name of the expense and the amount.  Added placeholder text. The code will be later on which what we're storing in the cloud.

--- Financify/View Controllers/DetailTableViewController.swift
+++ Financify/View Controllers/DetailTableViewController.swift
@@ -9,18 +9,58 @@
 import UIKit
 
 class DetailTableViewController: UITableViewController {
+    
+    // MARK: - IBActions
+    @IBAction func addButtonTapped(_ sender: UIBarButtonItem) {
+        showAlert()
+    }
 
+    // MARK: - View LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.reloadData()
     }
-
+    
+    // MARK: - Methods
+    func showAlert() {
+        let alert = UIAlertController(title: "New Expense", message: "Enter the expense name and the amount", preferredStyle: .alert)
+        
+        alert.addTextField()
+        alert.addTextField()
+        
+        alert.textFields![0].placeholder = "Expense Name"
+        alert.textFields![1].placeholder = "Amount"
+        alert.textFields![1].keyboardType = UIKeyboardType.decimalPad
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        alert.addAction(UIAlertAction(title: "Enter", style: .default, handler: { [weak self, weak alert] _ in
+            guard let expense = alert?.textFields?[0].text else { return }
+            guard let amount = alert?.textFields?[1].text, let doubleAmount = Double(amount) else { return }
+            self?.submitEntry(expense, doubleAmount)
+        }))
+        
+        self.present(alert, animated: true)
+    }
+    
+    func submitEntry(_ expense: String, _ amount: Double) {
+        // TODO - Expense and amount will go here
+        // Sample code: expenseName.insert(expense, at: 0)
+        // amountArray.append(amount)
+        tableView.reloadData()
+        return
+    }
+    
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return 0 //Name of the expense.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DetailCell", for: indexPath)
+        /*
+         cell.textLabel?.text = expenseName[indexPath.row]
+         cell.detailTextLabel?.text = "$\(amountArray[indexPath.row])"
+         */
+        return cell
     }
 }
